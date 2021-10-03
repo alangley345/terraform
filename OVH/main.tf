8e25fcec86535328b27ef248c2894dd6c569c7e6
Aaron Langley <alangley345@gmail.com>
2021-10-03 09:52:02 -0400

    updated provider and test

--- OVH/main.tf
+++ OVH/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "2.7.1"
+      version = "2.8.0"
     }
   }  
 }
@@ -10,5 +10,4 @@ terraform {
 provider "proxmox" {
   # Configuration options
   pm_api_url = "https://proxmox.greatcatlab.net:8006/api2/json"
-  region = "us=east-1"
 }
